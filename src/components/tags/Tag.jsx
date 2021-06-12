73d74f40d87920332001cd08d5d0e633761b671c
Colais <nicholasbalby@hotmail.com>
2021-06-12 10:12:47 -0300

    Tag dinamica.

--- src/components/tags/Tag.jsx
+++ src/components/tags/Tag.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TiDeleteOutline } from 'react-icons/ti'
 
 const Tag = ({ text, background_color, color_text }) => {
 
+    const [iconDelete, setIconDelete] = useState(false)
+
+    function searchByTag(text) {
+        console.log(text)
+        if (text && !iconDelete) {
+            setIconDelete(!iconDelete)
+        } else {
+            if (iconDelete) {
+                setIconDelete(!iconDelete)
+            }
+        }
+
+    }
+
     return (
-        <div className='container-tags__tag' style={{ backgroundColor: background_color, color: color_text }}>
+        <div className={`container-tags__tag ${iconDelete && 'container-tags__tag__focus'}`} style={{ backgroundColor: background_color, color: color_text }}
+            onClick={(e) => searchByTag(e.target.childNodes[0].data)}>
             {text}
+            {iconDelete && <span className='container-tags__tag__delete-icon'><TiDeleteOutline /></span>}
         </div>
     )
 }
