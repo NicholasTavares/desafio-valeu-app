0268ebfd472d49fbe81df90e1e092fe67e564f9a
Colais <nicholasbalby@hotmail.com>
2021-06-15 20:05:27 -0300

    Adicionando contador de pokemons.

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -8,12 +8,17 @@ const Cards = () => {
     const { pokemons } = usePokemon()
 
     return (
-        <main className='container-cards'>
-            {pokemons ? pokemons.map((res, i) => (
-                <Card name={res.name} img_url={res.img} type={res.type}
-                    height={res.height} weight={res.weight} candy={res.candy}
-                    candy_count={res.candy_count} weaknesses={res.weaknesses} avg_spawns={res.spawn_chance} key={i} />
-            )) : <Loading />}
+        <main className='container-cards-count'>
+            {pokemons && <p className='container-cards-count__count'>
+                {pokemons && `(${pokemons.length} results)`}
+            </p>}
+            <main className='container-cards'>
+                {pokemons ? pokemons.map((res, i) => (
+                    <Card name={res.name} img_url={res.img} type={res.type}
+                        height={res.height} weight={res.weight} candy={res.candy}
+                        candy_count={res.candy_count} weaknesses={res.weaknesses} avg_spawns={res.spawn_chance} key={i} />
+                )) : <Loading />}
+            </main>
         </main>
     )
 }
